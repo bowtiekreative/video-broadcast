d482933319e0ba84deac98d05e4397f3fbb3590a
Josh Lankford <lankford.josh@gmail.com>
2016-10-21 18:39:18 -0700

    build

--- src/js/components/Input.js
+++ src/js/components/Input.js
@@ -109,12 +109,12 @@ class ChatInput extends React.Component {
   }
 
   renderActions() {
-    const {creator} = this.props.creator;
-    if(!creator){ return }
-    return (<div className="controls-action" style={this.calculateCallBtnStyle()}>
-      <button className={`chat-input--actions--item call-btn ${this.props.calledIn ? 'hangup' : '' }` } onClick={this.props.callInHangUp}>
-      </button>
-    </div>);
+    if(this.props.creator){
+      return (<div className="controls-action" style={this.calculateCallBtnStyle()}>
+        <button className={`chat-input--actions--item call-btn ${this.props.calledIn ? 'hangup' : '' }` } onClick={this.props.callInHangUp}>
+        </button>
+      </div>);
+    }
   }
 
   render() {
